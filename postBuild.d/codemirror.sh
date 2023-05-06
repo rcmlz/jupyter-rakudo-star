5bd199019900e92077ab36e9cfb784ec2a137a66
rcmlz <19784049+rcmlz@users.noreply.github.com>
2023-05-06 19:28:57 +0200

    Update codemirror.sh

--- postBuild.d/codemirror.sh
+++ postBuild.d/codemirror.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}".format(*version))'`
+export PYTHON_VERSION=`python3 -c 'import sys; print("python" + ".".join(map(str, sys.version_info[:2])))'`
 
 HIGHLIGHTING=ressources/raku.js #or raku1.js if you like it more
 
-CM_RAKU="/srv/conda/envs/notebook/lib/python$PYTHON_VERSION/site-packages/notebook/static/components/codemirror/mode/raku"
+CM_RAKU="/srv/conda/envs/notebook/lib/$PYTHON_VERSION/site-packages/notebook/static/components/codemirror/mode/raku"
 mkdir $CM_RAKU
 
 cp $HIGHLIGHTING $CM_RAKU/raku.js
